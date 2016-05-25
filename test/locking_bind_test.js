3a7efc3f4a68f80609727c8d072688c4e2741863
Taka Okunishi <okunishitaka.com@gmail.com>
2016-05-25 14:49:04 +0900

    Lock/Unlockの仕組みを作った。

---
+++ test/locking_bind_test.js
@@ -0,0 +1,25 @@
+/**
+ * Test case for lockingBind.
+ * Runs with mocha.
+ */
+'use strict'
+
+const lockingBind = require('../lib/bindings/locking_bind.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('locking-bind', () => {
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Locking bind', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
--- test/sg_socket_test.js
+++ test/sg_socket_test.js
@@ -5,6 +5,7 @@
 'use strict'
 
 const sgSocket = require('../lib/sg_socket.js')
+const sgSocketClient = require('sg-socket-client')
 const assert = require('assert')
 const co = require('co')
 
@@ -18,11 +19,40 @@ describe('sg-socket', () => {
   }))
 
   it('Sg socket', () => co(function * () {
-    let server = sgSocket(9876)
+    let port = 9876
+    let server = sgSocket(port)
 
-    yield new Promise((resolve) =>
-      server.close(resolve())
-    )
+    let client01 = sgSocketClient(`http://localhost:${port}`)
+    yield new Promise((resolve) => client01.on('connect', () => resolve()))
+
+    let client02 = sgSocketClient(`http://localhost:${port}`)
+    yield new Promise((resolve) => client02.on('connect', () => resolve()))
+
+    client01.lock('hoge')
+    yield new Promise((resolve) => {
+      client02.on('sg:alloc', (data) => {
+        assert.equal(data.name, 'hoge')
+        resolve()
+      })
+    })
+
+    client02.lock('hoge')
+    yield new Promise((resolve) => {
+      client02.on('sg:conflict', (data) => {
+        assert.equal(data.name, 'hoge')
+        resolve()
+      })
+    })
+
+    client01.unlock('hoge')
+    yield new Promise((resolve) => {
+      client02.on('sg:release', (data) => {
+        assert.equal(data.name, 'hoge')
+        resolve()
+      })
+    })
+
+    yield new Promise((resolve) => server.close(resolve()))
   }))
 })
 
